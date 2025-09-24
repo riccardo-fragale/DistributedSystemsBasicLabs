ca73e7b7ea36c068e2fb3b10b926facddb208e8d
itsrich <ricfra32@gmail.com>
2025-09-24 19:04:01 +0200

    -feat: started HW4 up to part 2 completed

---
+++ HW4/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Start an Erlang node with short name "kista" and cookie "1234"
+# Then compile gms1, test, gui, worker.
+
+erl -sname kista -setcookie 1234 -eval "
+    compile:file(gms1),
+    compile:file(test),
+    compile:file(gui),
+    compile:file(worker).
+"
